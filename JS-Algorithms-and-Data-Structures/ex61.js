f80716aadf354045f4ef29c808899c76daf37609
unknown <lewisthagichu254@gmail.com>
2023-03-01 18:18:43 +0300

    Comparison with the Strict Equality Operator

---
+++ JS-Algorithms-and-Data-Structures/ex61.js
@@ -0,0 +1,10 @@
+// Comparison with the Strict Equality Operator
+
+function testStrict(val) {
+    if (val === 7) { 
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
