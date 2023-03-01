076567690d349f5aa94b60257373ee0ddf548501
unknown <lewisthagichu254@gmail.com>
2023-03-01 18:26:46 +0300

    Comparison with the Strict Inequality Operator

---
+++ JS-Algorithms-and-Data-Structures/ex64.js
@@ -0,0 +1,10 @@
+// Comparison with the Strict Inequality Operator
+
+function testStrictNotEqual(val) {
+    if (val !== 17) {
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testStrictNotEqual(10);
