9efd79828ab966ad0e23ab5bbbdb63ea4880c879
unknown <lewisthagichu254@gmail.com>
2023-03-06 16:39:57 +0300

    Use Multiple Conditional (Ternary) Operators

---
+++ JS-Algorithms-and-Data-Structures/ex111.js
@@ -0,0 +1,10 @@
+// Use Multiple Conditional (Ternary) Operators
+
+function checkSign(num) {
+    return (num > 0) ? "positive"
+    : (num < 0) ? "negative"
+    : "zero";
+  
+  }
+  
+  console.log(checkSign(10));
