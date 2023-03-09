e7466f95d248df22fc17225d790e20005513067f
unknown <lewisthagichu254@gmail.com>
2023-03-09 12:14:38 +0300

    prevent Object mutation

---
+++ ES6/ex3.js
@@ -0,0 +1,17 @@
+// Prevent Object Mutation
+
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    
+    Object.freeze(MATH_CONSTANTS); // added code
+    
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
