181dfc98679680fc820f8378b0bc3b0542142f76
unknown <lewisthagichu254@gmail.com>
2023-03-09 13:15:20 +0300

    Use the Rest Parameter with Function Parameters

---
+++ ES6/ex7.js
@@ -0,0 +1,10 @@
+// Use the Rest Parameter with Function Parameters
+
+const sum = (...args) => {
+    //const args = [x, y, z];
+    let total = 0;
+    for (let i = 0; i < args.length; i++) {
+      total += args[i];
+    }
+    return total;
+  }
