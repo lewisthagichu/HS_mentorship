91f6c22d1428d572cc33331192e5bd9683019068
unknown <lewisthagichu254@gmail.com>
2023-03-11 12:40:17 +0300

    Reuse JavaScript Code Using import

---
+++ ES6/ex22.js
@@ -0,0 +1,8 @@
+// Reuse JavaScript Code Using import
+
+import {uppercaseString, lowercaseString} from './string_functions.js';
+
+
+lowercaseString("WORLD!");
+let test = uppercaseString("hello");
+console.log(test)
