e370b29e0e3df29bc4d13fc629c677cd6037503b
unknown <lewisthagichu254@gmail.com>
2023-03-11 13:18:18 +0300

    Use * to Import Everything from a File

---
+++ ES6/ex23.js
@@ -0,0 +1,6 @@
+// Use * to Import Everything from a File
+
+import * as stringFunctions from "./string_functions.js";
+
+stringFunctions.uppercaseString("hello");
+stringFunctions.lowercaseString("WORLD!");
