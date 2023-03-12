0e8911977a180986117fc55cabc003d0c043d3ae
lewisthagichu <lewisthagichu254@gmail.com>
2023-03-12 15:20:58 +0300

    Combine Arrays with the Spread Operator

---
+++ JS-Algorithms-and-Data-Structures/Basic-Data-Structures/ex9.js
@@ -0,0 +1,10 @@
+// Combine Arrays with the Spread Operator
+
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ['learning', ...fragment, 'is', 'fun']
+    return sentence;
+  }
+  
+  console.log(spreadOut()); // [ 'learning', 'to', 'code', 'is', 'fun' ]
+  
